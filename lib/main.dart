73ef33f1507f7da27c1940decab81668d953db56
betterplaywon <kkgi1234@gmail.com>
2022-01-23 17:59:26 +0900

    Feat: making flutter project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //앱 구동 시작 명령어
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Center(
+        child:Container(width:50, height:50,color:Colors.blue)
+      )
+    );
+  }
+}
+
