81fdcb3f8638235805a3ad00b91c9268cd291fed
betterplaywon <kkgi1234@gmail.com>
2022-01-24 16:59:40 +0900

    Modified: basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,33 @@
 import 'package:flutter/material.dart';
-
-void main() {
+import 'package:contact/Widget/bottom_bar.dart';
+void main() =>
   //앱 구동 시작 명령어
-  runApp(const MyApp());
-}
+  runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  _MyAppState createState() => _MyAppState();
+}
+  class _MyAppState extends State<MyApp>{
+  late TabController controller;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
-        appBar: AppBar(title: Text('Making App')),
-        body: Align(
-          alignment: Alignment.center,
-          child: Container(
-            width: double.infinity, height:50,
-           margin: EdgeInsets.all(20),
-           decoration: BoxDecoration(
-             color: Colors.blue,
-             border: Border.all(color:Colors.lightBlue)
+      title: 'Pracflix',theme: ThemeData(brightness: Brightness.dark,
+        primaryColor: Colors.black, colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.white) ),
 
-           ),
-           child: Text('Hi'),
+//physics option 적용으로 scroll event 시도에도 고정되어있다.
+home: DefaultTabController(length: 4, child: Scaffold(body: TabBarView(physics: NeverScrollableScrollPhysics(),children: <Widget>[
+  SizedBox(child: SizedBox(child: SizedBox(child: Center(child: Text('home'),),))),
+  SizedBox(child: SizedBox(child: Center(child: Text('search'),),)),
+  SizedBox(child: SizedBox(child: Center(child: Text('save'),),)),
+  SizedBox(child: SizedBox(child: Center(child: Text('more'),),)),
+            ],
           ),
+        bottomNavigationBar: Bottom(),
         ),
-        bottomNavigationBar: BottomAppBar(
-          child:  Container(
-            width: 50,
-            height:50,
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [Icon(Icons.phone),Icon(Icons.message),Icon(Icons.contact_page)]
-            ),
-          ),
-        )
       )
     );
   }
 }
-
