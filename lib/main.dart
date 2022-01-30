ac2cd82dfb41b7915272100e5ba0f39a153f945c
betterplaywon <kkgi1234@gmail.com>
2022-01-30 16:19:11 +0900

    Modified: layout practice

--- lib/main.dart
+++ lib/main.dart
@@ -15,18 +15,40 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Test App')
+          title: Text('신길동'),
+          actions: const [
+            IconButton(onPressed: null,
+              icon: Icon(Icons.search),
+            ),
+            IconButton(onPressed: null,
+              icon: Icon(Icons.menu),
+            ),
+            IconButton(onPressed: null,
+              icon: Icon(Icons.notifications),
+            ),
+          ],
         ),
-        body: Text('Testdasdhasgdashdasgdjhagdaskhas'),
-        bottomNavigationBar: Container(
-          child: Align(
-            alignment: Alignment.bottomRight,
-            child: Container(
-            width: 50,height: 50,color: Colors.amberAccent,
+        body: SizedBox(
+         child: Row(
+          children: [
+            Image.asset('assets/TIL.png',width: 250,),
+            Column(
+              children: [
+                Text('Jordan 1 Retro High OG 급처'),
+                Text('신길동, 끌올 10분 전'),
+                Text('600,000원'),
+                Row(
+                  children: const [
+                    Icon(Icons.favorite),
+                    Text('4'),
+                  ],
+                ),
+              ],
             )
+          ],
+         ),
           ),
-        )
-      )
+        ),
     );
   }
 }
