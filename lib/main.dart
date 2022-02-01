38e53377fec8d01f24e2bfb6edc66f3a4b30fe63
betterplaywon <kkgi1234@gmail.com>
2022-02-01 22:45:03 +0900

    Modified: using state

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,52 @@
-import 'dart:html';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   //앱 구동 시작 명령어
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp>{
+  var a = 1;
+  var name = ['강지원','고태준','양진혁'];
+  var count = [0,0,0];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-      appBar: AppBar(),
+        floatingActionButton: FloatingActionButton(
+          child: Text(a.toString()),
+          onPressed: () {
+          setState(() {
+            a++;
+          });
+          },
+        ),
+      appBar: AppBar(
+        title: Text('TEST'),
+      ),
         body:ListView.builder(
-          itemCount: 30,
-        itemBuilder: (context,index){
+          itemCount: 3,
+        itemBuilder: (context,i){
           return ListTile(
-            leading: Icon(Icons.account_circle),
-            title: Text('강지원'),
+            leading: Text(count[i].toString()),
+            title: Text(name[i]),
+            trailing: OutlinedButton(
+              onPressed: (){
+                setState(() {
+                  count[i]++;
+                });
+              },
+              child: Text('COUNT BTN'),
+            ),
           );
         },
               ),
